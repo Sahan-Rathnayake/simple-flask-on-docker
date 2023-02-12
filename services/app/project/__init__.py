441335725b804719e56f9d87197ef73fd4739886
Sahan Rathnayake <smanjitha@gmail.com>
2023-02-12 18:12:59 +0530

    display message updated

--- services/app/project/__init__.py
+++ services/app/project/__init__.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1 style='color:black'><center>Hello Moresand!</center></h1>"
+    return "<h1 style='color:black'><center>Hello World!</center></h1>"
